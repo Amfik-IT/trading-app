12f5667a84b4cbae9d4345c6a76aa03070c3e6fb
Vladimir_Amfik <e.v.e.n@mail.ru>
2021-08-17 15:09:48 +0300

    carcass

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
-import TablesContainer from './components/TablesPage/TablesContainer';
+import TablesContainer from './components/Pages/TablesPage/TablesContainer';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Dashboard from './components/Pages/DashboardPage/Dashboard';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
-        <div className="app-wrapper-content">
+        <div class="main-content" id="panel">
           <Header />
-          <Route path="/tables" render={ () => <TablesContainer />} />
+          <div class="container-fluid mt--6">
+            <Route path="/dashboard" render={ () => <Dashboard /> } />
+            <Route path="/tables" render={ () => <TablesContainer />} />
+            <Footer />
+          </div>
         </div>
       </div>
     </BrowserRouter>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,27 +1,36 @@
 import React from 'react';
-import s from './Navbar.module.css';
 import { NavLink } from 'react-router-dom';
 import logo from "../../assets/img/brand/blue.png";
 
 const Navbar = () => {
     return (
-        <div className={s.navbar}>
-            <div className={s.navbarHeader}>
-                <a className={s.navbarLogo}>
-                    <img src={logo} className={s.navbarLogoImg} alt="..."/> 
-                </a>
+        <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+            <div class="scrollbar-inner">
+                <div class="sidenav-header  align-items-center">
+                    <a class="navbar-brand" href="/dashboard">
+                    <img src={logo} class="navbar-brand-img" alt="..."/>
+                    </a>
+                </div>
+                <div class="navbar-inner">
+                    <div class="collapse navbar-collapse" id="sidenav-collapse-main">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                            <NavLink to='/dashboard' activeClassName="active" class="nav-link" href="examples/dashboard.html">
+                                <i class="ni ni-tv-2 text-primary"></i>
+                                <span class="nav-link-text">Dashboard</span>
+                            </NavLink>
+                            </li>
+                            <li class="nav-item">
+                            <NavLink to='/tables' activeClassName="active" class="nav-link" href="examples/tables.html">
+                                <i class="ni ni-bullet-list-67 text-default"></i>
+                                <span class="nav-link-text">Tables</span>
+                            </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <nav className={s.nav}>
-                    <NavLink to='/dashboard' className={`${s.item}`} activeClassName={s.active}>
-                            <i className='ni ni-tv-2 text-primary'></i>
-                            <span className={s.navLinkText}>Dashboard</span>
-                    </NavLink>
-                    <NavLink to='/tables' className={`${s.item}`} activeClassName={s.active}>
-                        <i className='ni ni-bullet-list-67 text-default'></i>
-                        <span className={s.navLinkText}>Tables</span>
-                    </NavLink>
-            </nav>
-        </div>
+        </nav>
     );
 };
 
