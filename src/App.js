f56de3745ea79dcc0002de482d4aa7f052b9a85f
Vladimir_Amfik <e.v.e.n@mail.ru>
2021-08-18 13:29:01 +0300

    init_state

--- src/App.js
+++ src/App.js
@@ -5,8 +5,19 @@ import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Dashboard from './components/Pages/DashboardPage/Dashboard';
+import { useEffect } from 'react';
+import {loadingActionCreator, updateOperationActionCreator,errorActionCreator} from "./redux/operations-reducer"
 
-function App() {
+function App(props) {
+  
+  useEffect(() => {
+    props.store.dispatch(loadingActionCreator())
+    fetch("https://invest-dimasik.herokuapp.com/api/trades?limit=10")
+    .then(response => response.json())
+    .then(items => props.store.dispatch(updateOperationActionCreator(items)))
+    .catch(errors => props.store.dispatch(errorActionCreator(errors)))
+  },[])
+
   return (
     <BrowserRouter>
       <div className="App">
